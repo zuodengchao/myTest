096b5d2fcfb296e313a39134b71e94defef30054
zuodengchao <983625108@qq.com>
2018-06-16 12:25:29 +0800

    Update ServiceD.java

--- Service/src/main/java/com/zdc/service/ServiceD.java
+++ Service/src/main/java/com/zdc/service/ServiceD.java
@@ -6,6 +6,6 @@ import java.util.concurrent.ExecutionException;
  * Created by ZDC on 2018/5/8.
  */
 public interface ServiceD {
-    String  abc="abc";
+    String  abc="dddd";
     public  String getStr(String str) throws ExecutionException, InterruptedException;
 }
