36b0170c52f1b8ae7e74623811207a41014ab50b
zuodengchao <983625108@qq.com>
2018-06-16 11:17:57 +0800

    修改变量名称

--- Service/src/main/java/com/zdc/service/ServiceC.java
+++ Service/src/main/java/com/zdc/service/ServiceC.java
@@ -4,7 +4,7 @@ package com.zdc.service;
  * Created by ZDC on 2018/5/8.
  */
 public interface ServiceC {
-     int a=10;
+     int age=10;
     String name="abc";
     public  String getStr(String str);
 }
